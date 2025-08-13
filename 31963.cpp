54d295bf774b3208c7099057e8d67ff95ae5d1fc
yeojun1 <yeojun.lim@kakao.com>
2025-08-13 11:39:48 +0900

    31963

--- 31963.cpp
+++ 31963.cpp
@@ -1,13 +1,3 @@
-// from math import log2,ceil,floor
-// N = int(input())
-// A = list(map(int, input().split()))
-// res=[0]
-
-// for i in range(1,N):
-//     res.append(max(0,int(ceil(log2(A[i-1]/A[i])))+res[i-1]))
-
-// print(sum(res))
-
 #include <cstdio>
 #include <algorithm>
 #include <cmath>
